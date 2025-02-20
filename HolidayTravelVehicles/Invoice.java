5b9007e38a3aa15445d9895626a9499063294d89
liviby <107537662+liviby@users.noreply.github.com>
2025-02-20 17:40:24 +0700

    Update Invoice.java

--- HolidayTravelVehicles/Invoice.java
+++ HolidayTravelVehicles/Invoice.java
@@ -7,22 +7,25 @@ public class Invoice {
     private List<DealerInstalledOption> options;
     private double finalPrice;
 
-    public Invoice(String invoiceNumber, Customer customer, Vehicle vehicle, List<DealerInstalledOption> options, double finalPrice) {
+    public Invoice(String invoiceNumber, Customer customer, Vehicle vehicle, List<DealerInstalledOption> options) {
         this.invoiceNumber = invoiceNumber;
         this.customer = customer;
         this.vehicle = vehicle;
         this.options = options;
-        this.finalPrice = finalPrice;
+        this.finalPrice = calculateFinalPrice();
     }
 
-    public void displayInfo() {
-        System.out.println("Invoice Number: " + invoiceNumber);
-        customer.displayInfo();
-        vehicle.displayInfo();
-        System.out.println("Dealer Installed Options:");
+    public String getInvoiceNumber() { return invoiceNumber; }
+    public Customer getCustomer() { return customer; }
+    public Vehicle getVehicle() { return vehicle; }
+    public List<DealerInstalledOption> getOptions() { return options; }
+    public double getFinalPrice() { return finalPrice; }
+
+    private double calculateFinalPrice() {
+        double total = vehicle.getBaseCost();
         for (DealerInstalledOption option : options) {
-            option.displayInfo();
+            total += option.getPrice();
         }
-        System.out.println("Final Price: " + finalPrice + "฿");
+        return total;
     }
 }
