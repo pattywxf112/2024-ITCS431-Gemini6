d206a8e39a9076ac6adb044f5621e6cf262f8892
liviby <107537662+liviby@users.noreply.github.com>
2025-02-20 17:40:51 +0700

    Update Main.java

--- HolidayTravelVehicles/Main.java
+++ HolidayTravelVehicles/Main.java
@@ -7,9 +7,14 @@ public class Main {
         Customer myCustomer = new Customer("C001", "Vichai Yut", "Phuket", "0812345678");
         DealerInstalledOption option1 = new DealerInstalledOption("D001", "Sunroof", 45000);
         DealerInstalledOption option2 = new DealerInstalledOption("D002", "Leather Seats", 23000);
+        Invoice myInvoice = new Invoice("INV0001", myCustomer, myVehicle, Arrays.asList(option1, option2));
 
-        Invoice myInvoice = new Invoice("INV0001", myCustomer, myVehicle, Arrays.asList(option1, option2), 1268000);
-
-        myInvoice.displayInfo();
+        System.out.println("=== Vehicle Information ===");
+        System.out.println("Name: " + myVehicle.getName());
+        System.out.println("Model: " + myVehicle.getModel());
+        System.out.println("Year: " + myVehicle.getYear());
+        System.out.println("Manufacturer: " + myVehicle.getManufacturer());
+        System.out.println("Serial Number: " + myVehicle.getSerialNumber());
+        System.out.println("Base Cost: " + myVehicle.getBaseCost() + "฿");
     }
 }
